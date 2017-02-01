4de773bdfc0d60f6897f2a785d68cd9e1a7b4744
Tarik02 <Taras.Fomin@gmail.com>
2017-02-01 18:24:02 +0200

    Fix Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,6 +7,8 @@ SRC_PATH := $(realpath $(SRC_PATH))
 
 LOCAL_SRC_FILES_RAW := $(shell find $(SRC_PATH) -name '*.c') $(shell find $(SRC_PATH) -name '*.cpp')
 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES_RAW:$(SRC_PATH)/%=../src/%)
+LOCAL_SRC_FILES := $(filter-out ../src/regex/slre/unit_test.c, $(LOCAL_SRC_FILES))
+
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../src
 LOCAL_LDLIBS := -lstdc++
 
