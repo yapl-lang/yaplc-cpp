4c2a71dc6dbfee58940b0994cce69c6ef354b4d1
Tarik02 <Taras.Fomin@gmail.com>
2017-02-07 16:20:12 +0200

    Fix android makefile.

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,5 +3,6 @@ LOCAL_PATH := $(call my-dir)/..
 include $(CLEAR_VARS)
 
 include $(LOCAL_PATH)/project/yapl.mk
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(SRC_PATH)/%=src/%)
 
 include $(BUILD_EXECUTABLE)
