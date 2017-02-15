e199d24b8db62d0cbc7de36a28861fa71444d8ed
Tarik02 <Taras.Fomin@gmail.com>
2017-02-15 22:15:49 +0200

    Continue corking at Context.

--- src/yaplc/process/Context.h
+++ src/yaplc/process/Context.h
@@ -7,15 +7,30 @@ namespace yaplc { namespace process {
 	class Context {
 	public:
 		std::string package;
+		std::string path;
 		std::map<std::string, std::string> imports;
+		std::map<std::string, std::string> names;
 	
 	public:
 		Context() {
 			
 		}
 		
-		Context clone() {
-			return *this;
+		Context clone(const std::string &appender = "") {
+			Context clone;
+			
+			clone.package = package;
+			
+			if (appender == "") {
+				clone.path = path;
+			} else {
+				clone.path = path + "." + appender;
+			}
+			
+			clone.imports = imports;
+			clone.names = names;
+			
+			return clone;
 		}
 	};
 } }
