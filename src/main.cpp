c0af9a43fdafb1da20e83da04cc0130137c87c1f
Tarik02 <Taras.Fomin@gmail.com>
2017-03-05 11:00:37 +0200

    Remove try-catch in main.

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,32 @@
 #include "yaplc/NotReadyException.h"
 
 int main(int argc, char **argv) {
-	try {
-		yaplc::Compiler compiler;
-		
+	yaplc::Compiler compiler;
+
 #ifdef INPUT_FILE
-		std::string root(INPUT_FILE);
+	std::string root(INPUT_FILE);
 #else
-		std::string root(argv[1]);
+	std::string root(argv[1]);
 #endif
 
-		try {
-			//compiler.project(fs::relative(root).full_name());
-			compiler.project(fs::relative(root + "/../yapl").full_name());
+	try {
+		//compiler.project(fs::relative(root).full_name());
+		compiler.project(fs::relative(root + "/../yapl").full_name());
 
-			std::cout << "Compiling..." << std::endl;
-			compiler.compile();
+		std::cout << "Compiling..." << std::endl;
+		compiler.compile();
 
-			std::cout << "Processing..." << std::endl;
-			compiler.process();
+		std::cout << "Processing..." << std::endl;
+		compiler.process();
 
-			std::cout << "Emitting..." << std::endl;
-			compiler.emit();
+		std::cout << "Emitting..." << std::endl;
+		compiler.emit();
 
-			std::cout << "Building..." << std::endl;
-			compiler.build();
-		} catch (const yaplc::NotReadyException &) {
-			compiler.reportErrors(std::cout);
-		}
-	} catch (const std::exception &e) {
-		(std::cout << e.what() << std::endl).flush();
-
-		throw;
+		std::cout << "Building..." << std::endl;
+		compiler.build();
+	} catch (const yaplc::NotReadyException &) {
+		compiler.reportErrors(std::cout);
 	}
+
+	return 0;
 }
