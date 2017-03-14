d98cbcbc1dbc1725b0b38ce8de0b5266b72ffed5
Tarik02 <Taras.Fomin@gmail.com>
2017-03-14 16:55:04 +0200

    Fix constructor parsing.

--- src/yaplc/parser/MethodMemberParser.cpp
+++ src/yaplc/parser/MethodMemberParser.cpp
@@ -3,6 +3,7 @@
 #include "CodeOperatorParser.h"
 #include "TypeNameParser.h"
 #include "yaplc/structure/MethodMemberNode.h"
+#include "yaplc/structure/TypeNode.h"
 #include "CodeParser.h"
 #include "ArgumentsParser.h"
 
@@ -10,6 +11,13 @@ namespace yaplc { namespace parser {
 	void MethodMemberParser::handle(structure::MemberNode *parentNode, bool withoutBody) {
 		auto methodMemberNode = new structure::MethodMemberNode();
 		parentNode->set(methodMemberNode);
+
+		if (auto typeNode = parentNode->findParent<structure::TypeNode>()) {
+			if (typeNode->name->type == parentNode->getName()) {
+				parentNode->modifiers.emplace_back(parentNode->type->type, parentNode->type->getBegin(), parentNode->type->getEnd());
+				parentNode->type->type = "void";
+			}
+		}
 
 		if (!parse<ArgumentsParser>(methodMemberNode->arguments)) {
 			cancel();
