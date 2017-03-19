3267905bb77e9a387f345498fadc77964abbba9c
Tarik02 <Taras.Fomin@gmail.com>
2017-03-19 13:43:10 +0200

    Remove Makefile, jni.

--- jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)/..
-
-include $(CLEAR_VARS)
-
-include $(LOCAL_PATH)/project/yapl.mk
-LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(SRC_PATH)/%=src/%)
-
-include $(BUILD_EXECUTABLE)
