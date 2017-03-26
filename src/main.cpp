7851b87b36e29cf44a91637e48025f804919ab26
Tarik02 <Taras.Fomin@gmail.com>
2017-03-26 12:47:21 +0300

    Add show command.

--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,53 @@
 #include <iomanip>
 #include <algorithm>
 
+#include <binstream/binstream.h>
 #include <commandline/CommandLine.h>
+
 #include "yaplc/Compiler.h"
 #include "yaplc/NotReadyException.h"
+#include "yaplc/structure/NodeFactory.h"
 
 struct CommandEntry {
 	char shortName;
 	std::string name;
 
+	std::string args;
+
 	std::string excerpt;
 };
 
 static std::vector<CommandEntry> commands {
-	{'h', "help", "Display help information and list of commands."},
-	{'v', "version", "Display version information."}
+	{'h', "help", "", "Display help information and list of commands."},
+	{'v', "version", "", "Display version information."},
+	{'s', "show", "<file...>", "Prints specified object file."}
 };
 
 void show_help() {
-	std::cout << "Usage: yaplc [options] project" << std::endl;
+	std::cout << "Usage: yaplc [options] <project>" << std::endl;
 
 	std::cout << "Options:" << std::endl;
 	for (const auto &command : commands) {
 		std::string names;
-		names.reserve(command.name.size() + 2 + 1);
+		names.reserve(2 + command.name.size() +
+			2 + 1 + 1 +
+			1 + command.args.size());
 
 		if (command.shortName != '\0') {
+			names += '-';
 			names += command.shortName;
 
 			if (!command.name.empty()) {
-				names += ", ";
+				names += ", --";
 				names += command.name;
 			}
 		} else {
+			names += "--";
 			names += command.name;
+		}
+
+		if (!command.args.empty()) {
+			names += " " + command.args;
 		}
 
 		std::cout << "    " << std::setw(20) << std::left << names << " " << std::right << command.excerpt << std::endl;
@@ -71,6 +85,35 @@ int main(int argc, char *argv[]) {
 			return 0;
 		} else if (commandLine.param("version", 'v')) {
 			show_version();
+			return 0;
+		} else if (commandLine.param("show", 's')) {
+			bool hasShowed = false;
+
+			std::string file;
+			while (commandLine.next(file)) {
+				hasShowed = true;
+
+				auto path = fs::relative(file);
+				if (!path.exists()) {
+					std::cout << "File \"" << file << "\" does not exist." << std::endl;
+					continue;
+				}
+
+				auto node = yaplc::structure::NodeFactory::loadNode(binstream{path.content()});
+				if (node == nullptr) {
+					std::cout << "File \"" << file << "\" is not a valid object file." << std::endl;
+					continue;
+				}
+
+				std::cout << file << ":" << std::endl;
+				node->show(std::cout);
+			}
+
+			if (!hasShowed) {
+				show_help();
+				return 1;
+			}
+
 			return 0;
 		} else {
 			break;
